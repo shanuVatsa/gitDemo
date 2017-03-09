6892a43c108d0714ee19b9bacb23c7a8ddd2c8f0
Shanu Vatsa <shanuv@cybage.com>
2017-03-09 16:38:36 +0530

    Third commit

---
+++ src/main/java/com/cybage/model/student.java
@@ -0,0 +1,34 @@
+package com.cybage.model;
+
+/**
+ * Created by Administrator on 3/9/2017.
+ */
+public class student {
+    private int id;
+    private String name;
+    private String address;
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
