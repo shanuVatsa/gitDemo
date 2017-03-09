c360aac936ab143079d4bd6c832072e4691f589b
Shanu Vatsa <shanuv@cybage.com>
2017-03-09 16:46:31 +0530

    Fourth commit

--- src/main/java/com/cybage/controller/HomeController.java
+++ src/main/java/com/cybage/controller/HomeController.java
@@ -1,5 +1,8 @@
 package com.cybage.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * Created by Administrator on 3/9/2017.
  */
